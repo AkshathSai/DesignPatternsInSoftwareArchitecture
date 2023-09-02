aee670e984803a72c9e6d2c6a86c25dbe41f7e38
Akshath Sai, Pittala <akshathsai.pittala@outlook.com>
2023-09-02 19:52:48 +0530

    Code Refactoring

--- src/main/java/org/example/singleton/Main.java
+++ src/main/java/org/example/singleton/Main.java
@@ -8,18 +8,18 @@ public class Main {
     public static void main(String[] args) {
 
         NaiveSingleton naiveSingleton = NaiveSingleton.getInstance("Some value");
-        log.info("" + naiveSingleton.value);
+        log.info("{} ", naiveSingleton.value);
         NaiveSingleton naiveSingleton1 = NaiveSingleton.getInstance("Some other value");
-        log.info("" + naiveSingleton1.value);
+        log.info("{} ", naiveSingleton1.value);
 
         Thread thread1 = new Thread(() -> {
             Singleton singleTon1 = Singleton.getInstance("ABC");
-            log.info("" + singleTon1.value);
+            log.info("{} ", singleTon1.value);
         });
 
         Thread thread2 = new Thread(() -> {
             Singleton singleton2 = Singleton.getInstance("DEF");
-            log.info("" + singleton2.value);
+            log.info("{} ", singleton2.value);
         });
 
         thread1.start();
@@ -27,12 +27,12 @@ public class Main {
 
         Thread thread3 = new Thread(() -> {
             NaiveSingleton naiveSingleton2 = NaiveSingleton.getInstance("Yet another value");
-            log.info("" + naiveSingleton2.value);
+            log.info("{} ", naiveSingleton2.value);
         });
 
         Thread thread4 = new Thread(() -> {
             NaiveSingleton naiveSingleton3 = NaiveSingleton.getInstance("Gosh! Another value?");
-            log.info("" + naiveSingleton3.value);
+            log.info("{} ", naiveSingleton3.value);
         });
 
         thread3.start();
